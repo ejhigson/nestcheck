5cba958778cb4cc455a4311472ea08f880dfeb38
ejhigson <ejhigson@gmail.com>
2018-02-13 14:24:12 +0000

    improvements to logX diagram plotting and adding functionality to plot multiple runs on the same axis

--- nestcheck/plots.py
+++ nestcheck/plots.py
@@ -5,7 +5,7 @@ Functions for diagnostic plots of nested sampling runs.
 Includes functions for plotting empirical parameter estimation diagrams of the
 type described
 in Section 3.1 and Figure 3 of "Sampling errors in nested sampling parameter
-estimation" (Higson et al, 2018) for nest sampling runs.
+estimation" (Higson 2017) for nest sampling runs.
 """
 
 import numpy as np
@@ -21,11 +21,12 @@ matplotlib.rc('text', usetex=True)
 matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 
 
-def bootstrap_kde_plot(bs_df, labels=None, **kwargs):
+def bootstrap_kde_plot(bs_df, labels=None, xlims=None, **kwargs):
     """
     Plots distributions of bootstrap values for each estimator. There is one
     subplot for each estimator with all runs plotted on the same axis.
     """
+    assert xlims is None or isinstance(xlims, dict)
     figsize = kwargs.pop('figsize', (6.4, 1.5))
     if kwargs:
         raise TypeError('Unexpected **kwargs: %r' % kwargs)
@@ -41,22 +42,28 @@ def bootstrap_kde_plot(bs_df, labels=None, **kwargs):
         supmax = bs_df[col].apply(np.max).max()
         support = np.linspace(supmin - 0.1 * (supmax - supmin),
                               supmax + 0.1 * (supmax - supmin), 100)
-        for nrun, samps in enumerate(bs_df[col].values):
+        for samps in bs_df[col].values:
             kernel = scipy.stats.gaussian_kde(samps)
             pdf = kernel(support)
             pdf /= pdf.max()
             ax.plot(support, pdf)
         ax.set_ylim([0, 1.1])
         ax.set_yticks([])
+        if xlims is not None:
+            try:
+                ax.set_xlim(xlims[col])
+            except KeyError:
+                pass
         ax.set_xlabel(col)
-        if ax.is_first_col():
-            ax.set_ylabel('probability density')
+        # if ax.is_first_col():
+        #     ax.set_ylabel('probability density')
         # if ax.is_last_col():
         #     prune = None
         # else:
         #     prune = 'upper'
-        # ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(nbins=3,
+        # ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(nbins=2)
         #                                                          prune=prune))
+        ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(nbins=2))
     # fig.subplots_adjust(wspace=0)
     return fig
 
@@ -98,9 +105,9 @@ def plot_bs_dists(run, fthetas, axes, **kwargs):
     bs_even_samps = []
     for i in range(n_simulate):
         run_temp = ar.bootstrap_resample_run(run, threads=threads)
-        logw = ar.get_logw(run_temp, simulate=False)
-        w_rel = np.exp(logw - logw.max())
-        even_w_inds = np.where(w_rel > np.random.random(w_rel.shape))[0]
+        logw_temp = ar.get_logw(run_temp, simulate=False)
+        w_temp = np.exp(logw_temp - logw_temp.max())
+        even_w_inds = np.where(w_temp > np.random.random(w_temp.shape))[0]
         bs_even_samps.append(run_temp['theta'][even_w_inds, :])
     for nf, ftheta in enumerate(fthetas):
         # Make an array where each row contains one bootstrap replication's
@@ -117,8 +124,9 @@ def plot_bs_dists(run, fthetas, axes, **kwargs):
         y, pmf = fgivenx.compute_pmf(samp_kde, theta, samples_array, ny=ny,
                                      cache=cache, parallel=parallel)
         if flip_x:
-            cbar = fgivenx.plot.plot(y[::-1], theta, pmf.T, axes[nf],
-                                     colors=colormap, smooth=smooth)
+            cbar = fgivenx.plot.plot(y, theta, np.swapaxes(pmf, 0, 1),
+                                     axes[nf], colors=colormap,
+                                     smooth=smooth)
         else:
             cbar = fgivenx.plot.plot(theta, y, pmf, axes[nf],
                                      colors=colormap, smooth=smooth)
@@ -156,8 +164,9 @@ def bs_param_dists(run_list, **kwargs):
                              gridspec_kw={'wspace': 0.05,
                                           'width_ratios': width_ratios},
                              figsize=figsize)
-    colormaps = ['Blues_r', 'Reds_r', 'Greys_r', 'Greens_r', 'Oranges_r']
-    for nrun, run in enumerate(run_list):
+    colormaps = ['Reds_r', 'Blues_r', 'Greys_r', 'Greens_r', 'Oranges_r']
+    # plot in reverse order so reds are final plot and always on top
+    for nrun, run in reversed(list(enumerate(run_list))):
         if cache_in is not None:
             cache = cache_in + '_' + str(nrun)
         else:
@@ -194,11 +203,13 @@ def bs_param_dists(run_list, **kwargs):
     return fig
 
 
-def param_est_diagram(run, **kwargs):
+def param_est_diagram(run_list, **kwargs):
     """
     Creates parameter estimation diagrams using the
     settings module for likelihoods and the specified estimator.
     """
+    if not isinstance(run_list, list):
+        run_list = [run_list]
     cache_in = kwargs.pop('cache', None)
     parallel = kwargs.pop('parallel', True)
     # Use random seed to make samples consistent and allow caching.
@@ -209,7 +220,11 @@ def param_est_diagram(run, **kwargs):
     smooth_logx = kwargs.pop('smooth_logx', True)
     scatter_plot = kwargs.pop('scatter_plot', True)
     n_simulate = kwargs.pop('n_simulate', 100)
-    threads_to_plot = kwargs.pop('threads_to_plot', [0, 1, 2])
+    if len(run_list) == 1:
+        threads_to_plot = kwargs.pop('threads_to_plot', [0, 1, 2])
+    else:
+        threads_to_plot = kwargs.pop('threads_to_plot', [])
+    plot_means = kwargs.pop('plot_means', True)
     fthetas = kwargs.pop('fthetas', [lambda theta: theta[:, 0],
                                      lambda theta: theta[:, 1]])
     labels = kwargs.pop('labels', [r'$\theta_' + str(i + 1) + '$' for i in
@@ -217,113 +232,137 @@ def param_est_diagram(run, **kwargs):
     ftheta_lims = kwargs.pop('ftheta_lims', [[-1, 1]] * len(fthetas))
     npoints = kwargs.pop('npoints', 100)
     logx_min = kwargs.pop('logx_min', None)
-    ftheta_sups = [np.linspace(lim[0], lim[1], npoints) for lim in ftheta_lims]
     nlogx = kwargs.pop('nlogx', npoints)
-    # make figure
+    ny_posterior = kwargs.pop('ny_posterior', npoints)
     figsize = kwargs.pop('figsize', (6.4, 2 * (1 + len(fthetas))))
     if kwargs:
         raise TypeError('Unexpected **kwargs: %r' % kwargs)
+    assert len(fthetas) == len(labels)
+    assert len(fthetas) == len(ftheta_lims)
+    # make figure
+    ftheta_sups = [np.linspace(lim[0], lim[1], npoints) for lim in ftheta_lims]
     fig, axes = plt.subplots(nrows=1 + len(fthetas), ncols=2, figsize=figsize,
                              gridspec_kw={'wspace': 0,
                                           'hspace': 0,
                                           'width_ratios': [15, 40]})
     axes[0, 0].set_visible(False)
-    # Weight Plot
-    # -------------------------
-    ax_weight = axes[0, 1]
-    ax_weight.set_ylabel('posterior\nmass')
-    samples = np.zeros((n_simulate, run['nlive_array'].shape[0] * 2))
-    for i in range(n_simulate):
-        logx_temp = ar.get_logx(run['nlive_array'], simulate=True)[::-1]
-        logw_rel = logx_temp + run['logl'][::-1]
-        w_rel = np.exp(logw_rel - logw_rel.max())
-        w_rel /= np.trapz(w_rel, x=logx_temp)
-        samples[i, ::2] = logx_temp
-        samples[i, 1::2] = w_rel
-    if logx_min is None:
-        logx_min = samples[:, 0].min()
-    logx_sup = np.linspace(logx_min, 0, nlogx)
-    ax_weight.set_xlim([logx_min, 0])
-    ax_weight.set_yticks([])
-    ax_weight.set_xticklabels([])
-    if cache_in is not None:
-        cache = cache_in + '_weights'
-    else:
-        cache = cache_in
-    y, pmf = fgivenx.compute_pmf(interp_alternate, logx_sup, samples,
-                                 cache=cache, ny=npoints, parallel=parallel)
-    cbar = fgivenx.plot.plot(logx_sup, y, pmf, ax_weight,
-                             colors=plt.get_cmap('Reds_r'))
-    # color bar plot
-    # --------------
-    divider = mpl_toolkits.axes_grid1.make_axes_locatable(axes[0, 0])
-    colorbar_ax = divider.append_axes("left", size="25%")
-    # spacing = divider.append_axes("left", size="50%")
-    # spacing.set_visible(False)
-    colorbar_plot = plt.colorbar(cbar, cax=colorbar_ax, ticks=[1, 2, 3])
-    colorbar_ax.yaxis.set_ticks_position('left')
-    colorbar_plot.solids.set_edgecolor('face')
-    colorbar_plot.ax.set_yticklabels(
-        [r'$1\sigma$', r'$2\sigma$', r'$3\sigma$'])
-    # samples plot
-    # ------------
-    run['logx'] = ar.get_logx(run['nlive_array'], simulate=False)
-    logw_expected = ar.get_logw(run, simulate=False)
-    w_rel = np.exp(logw_expected - logw_expected.max())
-    w_rel /= np.sum(w_rel)
-    means = [np.sum(w_rel * ftheta(run['theta'])) for ftheta in fthetas]
-    # Plot samples
-    # ------------
-    threads = [ar.dict_given_samples_array(thread) for thread in
-               ar.get_run_threads(run)]
-    # thread_arrays = ar.get_run_threads(run)
-    # threads = [ar.dict_given_samples_array(thread) for thread in
-    #            thread_arrays]
-    for nf, ftheta in enumerate(fthetas):
-        ax_samples = axes[1 + nf, 1]
-        for i in threads_to_plot:
-            ax_samples.plot(threads[i]['logx'], ftheta(threads[i]['theta']),
-                            color='black', lw=1)
-        if scatter_plot:
-            ax_samples.scatter(run['logx'], ftheta(run['theta']), s=0.4,
-                               color='red')
+    colors = ['red', 'blue', 'grey', 'green', 'orange']
+    colormaps = ['Reds_r', 'Blues_r', 'Greys_r', 'Greens_r', 'Oranges_r']
+    # plot in reverse order so reds are done last and on top
+    for nrun, run in reversed(list(enumerate(run_list))):
+        if cache_in is not None:
+            if nrun != len(run_list) - 1:
+                cache_in = cache_in[:-2]
+            cache_in += '_' + str(nrun)
+        # Weight Plot
+        # -------------------------
+        ax_weight = axes[0, 1]
+        ax_weight.set_ylabel('posterior\nmass')
+        samples = np.zeros((n_simulate, run['nlive_array'].shape[0] * 2))
+        for i in range(n_simulate):
+            logx_temp = ar.get_logx(run['nlive_array'], simulate=True)[::-1]
+            logw_rel = logx_temp + run['logl'][::-1]
+            w_rel = np.exp(logw_rel - logw_rel.max())
+            w_rel /= np.trapz(w_rel, x=logx_temp)
+            samples[i, ::2] = logx_temp
+            samples[i, 1::2] = w_rel
+        # For absolute weights
+        # logx_list = []
+        # logw_list = []
+        # for i in range(n_simulate):
+        #     logx_temp = ar.get_logx(run['nlive_array'], simulate=True)[::-1]
+        #     logx_list.append(logx_temp)
+        #     logw_list.append(logx_temp + run['logl'][::-1])
+        # logw_max = np.hstack(logw_list).max()
+        # for i in range(n_simulate):
+        #     samples[i, ::2] = logx_list[i]
+        #     samples[i, 1::2] = np.exp(logw_list[i] - logw_max)
+        # if logx_min is None:
+        #     logx_min = samples[:, 0].min()
+        logx_sup = np.linspace(logx_min, 0, nlogx)
+        if cache_in is not None:
+            cache = cache_in + '_weights'
         else:
-            if cache_in is not None:
-                cache = cache_in + '_param_' + str(nf)
+            cache = cache_in
+        y, pmf = fgivenx.compute_pmf(interp_alternate, logx_sup, samples,
+                                     cache=cache, ny=npoints,
+                                     parallel=parallel)
+        cbar = fgivenx.plot.plot(logx_sup, y, pmf, ax_weight,
+                                 colors=plt.get_cmap(colormaps[nrun]))
+        ax_weight.set_xlim([logx_min, 0])
+        ax_weight.set_ylim(bottom=0)
+        ax_weight.set_yticks([])
+        ax_weight.set_xticklabels([])
+        # color bar plot
+        # --------------
+        divider = mpl_toolkits.axes_grid1.make_axes_locatable(axes[0, 0])
+        colorbar_ax = divider.append_axes("left", size="10%")
+        # spacing = divider.append_axes("left", size="50%")
+        # spacing.set_visible(False)
+        colorbar_plot = plt.colorbar(cbar, cax=colorbar_ax, ticks=[1, 2, 3])
+        colorbar_ax.yaxis.set_ticks_position('left')
+        colorbar_plot.solids.set_edgecolor('face')
+        colorbar_plot.ax.set_yticklabels(
+            [r'$1\sigma$', r'$2\sigma$', r'$3\sigma$'])
+        # samples plot
+        # ------------
+        run['logx'] = ar.get_logx(run['nlive_array'], simulate=False)
+        threads = [ar.dict_given_samples_array(thread) for thread in
+                   ar.get_run_threads(run)]
+        for nf, ftheta in enumerate(fthetas):
+            ax_samples = axes[1 + nf, 1]
+            for i in threads_to_plot:
+                ax_samples.plot(threads[i]['logx'],
+                                ftheta(threads[i]['theta']),
+                                color='black', lw=1)
+            if scatter_plot:
+                ax_samples.scatter(run['logx'], ftheta(run['theta']), s=0.2,
+                                   color=colors[nrun])
             else:
-                cache = cache_in
-            # try sampling and combining batches
-            # batches = []
-            # for i in range(2000):
-            #     inds = np.random.randint(0, len(thread_arrays), 4)
-            #     temp = ar.combine_threads([thread_arrays[j] for j in inds],
-            #                               run['thread_min_max'][inds])
-            #     batches.append(temp)
-            # threads = batches
-            max_nsamp = max([thread['logx'].shape[0] for thread in threads])
-            samples = np.full((len(threads), 2 * max_nsamp), np.nan)
-            for i, thread in enumerate(threads):
-                nsamp = thread['logx'].shape[0]
-                samples[i, :2 * nsamp:2] = thread['logx'][::-1]
-                samples[i, 1:2 * nsamp:2] = ftheta(thread['theta'])[::-1]
-            y, pmf = fgivenx.compute_pmf(interp_alternate, logx_sup, samples,
-                                         y=ftheta_sups[nf], cache=cache)
-            _ = fgivenx.plot.plot(logx_sup, y, pmf, ax_samples,
-                                  smooth=smooth_logx)
-        ax_samples.set_xlim([logx_min, 0])
-        ax_samples.set_ylim(ftheta_lims[nf])
-        ax_samples.axhline(y=means[nf], lw=1, linestyle='--', color='black')
-    # Plot posteriors
-    # ---------------
-    posterior_axes = [axes[i + 1, 0] for i in range(len(fthetas))]
-    _ = plot_bs_dists(run, fthetas, posterior_axes,
-                      ftheta_lims=ftheta_lims,
-                      flip_x=True, n_simulate=n_simulate,
-                      cache=cache_in, nx=npoints, ny=npoints,
-                      parallel=parallel)
+                if cache_in is not None:
+                    cache = cache_in + '_param_' + str(nf)
+                else:
+                    cache = cache_in
+                max_samp = max([thread['logx'].shape[0] for thread in threads])
+                samples = np.full((len(threads), 2 * max_samp), np.nan)
+                for i, thread in enumerate(threads):
+                    nsamp = thread['logx'].shape[0]
+                    samples[i, :2 * nsamp:2] = thread['logx'][::-1]
+                    samples[i, 1:2 * nsamp:2] = ftheta(thread['theta'])[::-1]
+                y, pmf = fgivenx.compute_pmf(interp_alternate, logx_sup,
+                                             samples, y=ftheta_sups[nf],
+                                             cache=cache)
+                _ = fgivenx.plot.plot(logx_sup, y, pmf, ax_samples,
+                                      smooth=smooth_logx)
+            ax_samples.set_xlim([logx_min, 0])
+            ax_samples.set_ylim(ftheta_lims[nf])
+        # Plot posteriors
+        # ---------------
+        posterior_axes = [axes[i + 1, 0] for i in range(len(fthetas))]
+        _ = plot_bs_dists(run, fthetas, posterior_axes,
+                          ftheta_lims=ftheta_lims,
+                          flip_x=True, n_simulate=n_simulate,
+                          cache=cache_in, nx=npoints, ny=ny_posterior,
+                          colormap=colormaps[nrun],
+                          parallel=parallel)
+        # Plot means
+        # ---------------
+        if plot_means:
+            logw_expected = ar.get_logw(run, simulate=False)
+            w_rel = np.exp(logw_expected - logw_expected.max())
+            w_rel /= np.sum(w_rel)
+            means = [np.sum(w_rel * f(run['theta'])) for f in fthetas]
+            if len(run_list) == 1:
+                color = 'black'
+            else:
+                color = 'dark' + colors[nrun]
+            for nf, mean in enumerate(means):
+                for ax in [axes[nf + 1, 0], axes[nf + 1, 1]]:
+                    ax.axhline(y=mean, lw=1, linestyle='--', color=color)
+    # Format axes (only do ones even if there are multiple runs
     for nf, ax in enumerate(posterior_axes):
         ax.set_ylim(ftheta_lims[nf])
-        ax.axhline(y=means[nf], lw=1, linestyle='--', color='black')
+        ax.invert_xaxis()  # only invert once, not for ever run!
     axes[-1, 1].set_xlabel(r'$\log X$')
     # Add labels
     for i, label in enumerate(labels):
