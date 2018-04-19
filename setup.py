6e85582f47d3288cd238d6252fee4908b534fb53
ejhigson <ejhigson@gmail.com>
2018-04-19 10:42:53 +0100

    adding setup.cfg and updating setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python
-"""nestcheck setup."""
+"""
+nestcheck setup module.
+
+Based on https://github.com/pypa/sampleproject/blob/master/setup.py.
+"""
+import os
 import setuptools
+
+
+def get_long_description():
+    """Get the long description from the README file."""
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'README.md')) as readme_file:
+        long_description = readme_file.read()
+    return long_description
 
 
 setuptools.setup(name='nestcheck',
                  version='0.0.0',
+                 description=('Error analysis, diagnostic tests and plots for '
+                              'nested sampling calculations.'),
+                 long_description=get_long_description(),
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/ejhigson/nestcheck',
                  author='Edward Higson',
                  author_email='ejhigson@gmail.com',
-                 description=('Error analysis, diagnostic tests and plots for '
-                              'nested sampling calculations'),
-                 url='https://github.com/ejhigson/nestcheck',
+                 license='MIT',
+                 keywords='nested-sampling sampling error-analysis',
+                 classifiers=[  # Optional
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Science/Research',
+                     'Topic :: Software Development :: Build Tools',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 2',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'Topic :: Scientific/Engineering :: Astronomy',
+                     'Topic :: Scientific/Engineering :: Physics',
+                     'Topic :: Scientific/Engineering :: Visualization',
+                     'Topic :: Scientific/Engineering :: Information Analysis',
+                 ],
+                 packages=['nestcheck'],
                  # futures is in standard library for python >= 3.2, but
-                 # include it for backwards compatibility
+                 # include it for compatibility with python 2.7.
                  install_requires=['numpy>=1.13',
                                    'scipy>=1.0.0',
                                    'matplotlib>=2.1.0',
@@ -25,4 +59,6 @@ setuptools.setup(name='nestcheck',
                  # https://github.com/rtfd/readthedocs.org/issues/3148
                  extras_require={'docs': ['sphinx<1.6', 'numpydoc',
                                           'sphinx-rtd-theme', 'nbsphinx']},
-                 packages=['nestcheck'])
+                 project_urls={  # Optional
+                     'Docs': 'http://nestcheck.readthedocs.io/en/latest/',
+                     'Source': 'https://github.com/ejhigson/nestcheck'})
